719024608ac1587844a10aad0f665b2cfa088e30
Trisha0601 <talakokkulatrisha@gmail.com>
2024-10-23 09:32:08 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
-hello world
- This text was added originally while in the feature branch
+# Calculate the square of a number
+def square(num):
+    return num**2
+
+# Test the function
+print(square(5))
+
+This text was aded originally while in the feature branch
+This text was added originally while in the feature branch
